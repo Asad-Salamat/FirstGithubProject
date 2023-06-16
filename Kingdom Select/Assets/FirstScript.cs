d9b06e9783fec088e6f27964cb044f4ef7a0adf3
Asad-Salamat <114930587+Asad-Salamat@users.noreply.github.com>
2023-06-16 13:02:43 +0500

    SecondCommit

--- Kingdom Select/Assets/FirstScript.cs
+++ Kingdom Select/Assets/FirstScript.cs
@@ -7,7 +7,9 @@ public class FirstScript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        Debug.Log("Second commit");
+        print("Dont include this");
+        print("Include this");
     }
 
     // Update is called once per frame
